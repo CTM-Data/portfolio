7326da9dfd372343457b0e692013f9d09710d3d4
Clay Marquardt <clayton.marquardt@gmail.com>
2024-10-04 22:00:14 -0400

    refactored regression, streamlined via functions and imports, changed feature set per position. now starting work on option 2 of cli - set my lineup

--- fantasy_football/regression_cli/main.py
+++ fantasy_football/regression_cli/main.py
@@ -1,68 +1,39 @@
 import pandas as pd
 import train_model
 
-
-positions = [
-    ('QB', ['PassAtt_g', 'PassYds_g', 'RushAtt_g', 'ScrmYds_g']), 
-    ('RB', ['RushAtt_g', 'Targets_g', 'Catches_g', 'ScrmYds_g']), 
-    ('WR', ['Targets_g', 'Catches_g', 'ScrmYds_g']), 
-    ('TE', ['Targets_g', 'Catches_g', 'ScrmYds_g'])
-]
-year = 2024
-url = rf'https://www.pro-football-reference.com/years/{year}/fantasy.htm#fantasy'
-df = pd.read_html(url, skiprows=0)[0]
-
-df['Year'] = year
-df.columns = [f'{tup[0]}_{tup[1]}' if 'Unnamed' not in tup[0] else f'{tup[1]}' for tup in df.columns]
-df.Player = df.Player.str.replace('*', '').str.replace('+', '')
-
-mask = df.loc[df['Rk'] == 'Rk'].index
-df = df.drop(labels=mask)
-
-numeric_cols = [col for col in df.columns if col not in ['Player', 'Tm', 'FantPos']]
-for col in numeric_cols: 
-    df.loc[:, col] = pd.to_numeric(df[col])
-
-df = df.loc[df.Games_G != 0].reset_index(drop=True)
-df['PassAtt_g'] = df.Passing_Att / df.Games_G
-df['PassYds_g'] = df.Passing_Yds / df.Games_G
-df['RushAtt_g'] = df.Rushing_Att / df.Games_G
-df['Targets_g'] = df.Receiving_Tgt / df.Games_G
-df['Catches_g'] = df.Receiving_Rec / df.Games_G
-df['ScrmYds_g'] = (df.Rushing_Yds + df.Receiving_Yds) / df.Games_G
-df['PPR_g'] = df.Fantasy_PPR / df.Games_G
-
-model_dict = train_model.get_ff_model()
-
-#TODO: retest this script after resolving the NAN issue in train_model
-# add projection column, referencing model_dict
-df['Projection'] = 0
-for pos, cols in positions:
-    df.loc[df.FantPos == pos, 'Projection'] = model_dict[pos].predict([df.loc[df.FantPos == pos, cols]])
-# df['PPR_Projection'] = df.apply(lambda row: model_dict[row.FantPos].predict([[row.Rushing_Att_g, row.Catches_g, row.ScrmYds_g]]) if row.FantPos in model_dict.keys() else None, axis=1)
-df.loc[:, 'Projection'] = df['Projection'].astype(float)
+constraints = {'QB': 1, 'RB': 2, 'WR': 2, 'TE': 1, 'FLEX': 2}
 def main():
+    df = train_model.make_predictions()
+    df['Is_Flex'] = df.FantPos.isin(['RB', 'WR', 'TE'])
+    my_team = pd.read_csv('my_team.csv')
+    df['My_Team'] = df.Player.isin(my_team.Player.str.title())
     running = True
     while running:
         print('''
         MODES:
-        1. Single Projection
+        1. Show projection(s)
         2. Set my lineup
         3. Assess waivers
         4. Generate trade suggestions    
         ''')
-        print('')
         choice = input('Select your mode (enter a number): ')
 
         if choice == '1': 
             names = input('Enter the name(s) of your player(s) separated by commas: ')
             names = [name.title() for name in names.split(', ')]
-            print(df.loc[df.Player.isin(names), ['Player', 'FantPos', 'Rushing_Att_g', 'Catches_g', 'ScrmYds_g', 'PPR_Projection']].sort_values(by=['FantPos', 'PPR_Projection'], ascending=[True, False]))
+            res = df.loc[df.Player.isin(names), ['Player', 'FantPos', 'Prediction']].sort_values(by=['FantPos', 'Prediction'], ascending=[True, False]).reset_index(drop=True)
+            print(res)
             exit = input('Exit program? (Y/N) ')
             if exit.upper() == 'Y':
                 running = False
         elif choice == '2': 
-            pass
+            # choices = df.loc[(df.My_Team == True), ['Player', 'FantPos', 'Prediction']].sort_values(by=['FantPos', 'Prediction'], ascending=[True, False])
+            # lineup = {'QB': [], 'RB': [], 'WR': [], 'TE': [], 'FLEX': []}
+            # for pos, limit in constraints.items():
+            #     count = 0
+            #     while count < limit:
+            #         lineup[pos].append()
+            pass                    
         elif choice == '3': 
             pass
         elif choice == '4': 
--- fantasy_football/regression_cli/train_model.py
+++ fantasy_football/regression_cli/train_model.py
@@ -1,18 +1,41 @@
-def get_ff_model(): 
-    import pandas as pd
-    import warnings
-    from sklearn.linear_model import LinearRegression
-    from sklearn.model_selection import train_test_split
-    warnings.filterwarnings('ignore') 
+import pandas as pd
+import warnings
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+warnings.filterwarnings('ignore') 
 
-    positions = [
-        ('QB', ['PassAtt_g', 'PassYds_g', 'RushAtt_g', 'ScrmYds_g']), 
-        ('RB', ['RushAtt_g', 'Targets_g', 'Catches_g', 'ScrmYds_g']), 
-        ('WR', ['Targets_g', 'Catches_g', 'ScrmYds_g']), 
-        ('TE', ['Targets_g', 'Catches_g', 'ScrmYds_g'])
-    ]
-    
-    df = pd.read_csv('./training_data/ff_training_2017_2023.csv')
+positions = [
+    ('QB', 24, ['PassAtt_g', 'PassYds_g', 'RushAtt_g', 'ScrmYds_g']), 
+    ('RB', 60, ['RushAtt_g', 'Targets_g', 'Catches_g', 'ScrmYds_g']), 
+    ('WR', 72, ['Targets_g', 'Catches_g', 'ScrmYds_g']), 
+    ('TE', 24, ['Targets_g', 'Catches_g', 'ScrmYds_g'])
+]
+
+past_years = [2017, 2023]
+current_year = past_years[-1] + 1
+
+def get_training_data(start_year, end_year):
+    years = list(range(start_year, end_year+1))
+    df = pd.DataFrame()
+
+    for year in years:
+        url = rf'https://www.pro-football-reference.com/years/{year}/fantasy.htm#fantasy'
+        df_temp = pd.read_html(url, skiprows=0)[0]
+        df_temp.columns = [f'{tup[0]}_{tup[1]}' if 'Unnamed' not in tup[0] else f'{tup[1]}' for tup in df_temp.columns]
+        df_temp.Player = df_temp.Player.str.replace('*', '').str.replace('+', '')
+        mask = df_temp.loc[df_temp['Rk'] == 'Rk'].index
+        df_temp = df_temp.drop(labels=mask)
+        exclude = ['Player', 'Tm', 'FantPos']
+        columns = [col for col in df_temp.columns if col not in exclude]
+        for col in columns: 
+            df_temp.loc[:, col] = pd.to_numeric(df_temp[col])
+        df_temp = df_temp.loc[df_temp.Games_G != 0]
+        df_temp['Year'] = year
+        df = pd.concat([df, df_temp], ignore_index=True)
+    return df
+
+def train_model():    
+    df = get_training_data(past_years[0], past_years[-1])
     df['PassAtt_g'] = df.Passing_Att / df.Games_G
     df['PassYds_g'] = df.Passing_Yds / df.Games_G
     df['RushAtt_g'] = df.Rushing_Att / df.Games_G
@@ -20,14 +43,54 @@ def get_ff_model():
     df['Catches_g'] = df.Receiving_Rec / df.Games_G
     df['ScrmYds_g'] = (df.Rushing_Yds + df.Receiving_Yds) / df.Games_G
     df['PPR_g'] = df.Fantasy_PPR / df.Games_G
-    #TODO: need to find a way to remove NAs from select columns given the position.
+
     models = {}
-    for pos, cols in positions:
-        x = df.loc[df.FantPos == pos, cols].reset_index(drop=True)
-        y = df.loc[df.FantPos == pos, 'PPR_g'].reset_index(drop=True)
+    for pos, cutoff, cols in positions:
+        all_cols = cols + ['PPR_g']
+        # df_temp = df.dropna(subset=all_cols, how='any').reset_index(drop=True)
+        df_temp = df.fillna(0)
+        x = df_temp.loc[(df.FantPos == pos) & (df.Fantasy_PosRank <= cutoff), cols]
+        y = df_temp.loc[(df.FantPos == pos) & (df.Fantasy_PosRank <= cutoff), 'PPR_g']
         X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=42)
 
         models[pos] = LinearRegression()
         models[pos].fit(X_train, y_train)
-        print(f'R-squared for {pos}: {model_dict[pos].score(X_train, y_train)}')
-    return model_dict
+        print(f'R-squared for {pos}: {models[pos].score(X_train, y_train)}')
+    return models
+
+def get_current_data(year):
+    url = rf'https://www.pro-football-reference.com/years/{year}/fantasy.htm#fantasy'
+    df = pd.read_html(url, skiprows=0)[0]
+    df.columns = [f'{tup[0]}_{tup[1]}' if 'Unnamed' not in tup[0] else f'{tup[1]}' for tup in df.columns]
+    df.Player = df.Player.str.replace('*', '').str.replace('+', '')
+    mask = df.loc[df['Rk'] == 'Rk'].index
+    df = df.drop(labels=mask)
+    exclude = ['Player', 'Tm', 'FantPos']
+    columns = [col for col in df.columns if col not in exclude]
+    for col in columns: 
+        df.loc[:, col] = pd.to_numeric(df[col])
+    df = df.loc[df.Games_G != 0]
+    df['Year'] = year
+    df['PassAtt_g'] = df.Passing_Att / df.Games_G
+    df['PassYds_g'] = df.Passing_Yds / df.Games_G
+    df['RushAtt_g'] = df.Rushing_Att / df.Games_G
+    df['Targets_g'] = df.Receiving_Tgt / df.Games_G
+    df['Catches_g'] = df.Receiving_Rec / df.Games_G
+    df['ScrmYds_g'] = (df.Rushing_Yds + df.Receiving_Yds) / df.Games_G
+    df['PPR_g'] = df.Fantasy_PPR / df.Games_G
+    return df
+
+def make_predictions():
+    df_raw = get_current_data(current_year)
+    models = train_model()
+    df = pd.DataFrame()
+    for pos, cutoff, cols in positions:
+        df_temp = df_raw.loc[df_raw.FantPos == pos]
+        df_temp['Prediction'] = models[pos].predict(df_temp[cols])
+        df = pd.concat([df, df_temp])
+    return df
+
+
+
+        
+
